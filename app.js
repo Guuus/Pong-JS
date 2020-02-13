8f643011e82a73df103e5b20d7f27a7fad4db287
Gus <gduigou29@gmail.com>
2020-02-13 21:04:37 +0100

    feat: move the ball on the x and y axis

--- app.js
+++ app.js
@@ -23,3 +23,47 @@ const player2 = new Player("player2");
 document.addEventListener('keydown', function (e) {
     player1.move(e.keyCode);
 });
+
+/* Moving the ball */
+var ball = document.getElementById("ball");
+var ballLeft = ball.offsetLeft;
+var ballTop = ball.offsetTop;
+var dirLeft = "up";
+var dirTop = "down";
+var animId = null;
+
+function move() {
+
+    // Move the ball in the good direction on the y axis
+    if (ballLeft > 1345 && ballLeft < 1353) {
+        dirLeft = "down";
+    } else if (ballLeft == 0) {
+        dirLeft = "up";
+    }
+    
+    if (dirLeft == "up") {
+        ballLeft += 4;
+    } else if (dirLeft == "down") {
+        ballLeft -= 4;
+    }
+
+    // Move the ball in the good direction on the x axis
+    if (ballTop > 645 && ballTop < 653) {
+        dirTop = "up";
+    } else if (ballTop == 0) {
+        dirTop = "down";
+    }
+
+    if (dirTop == "up") {
+        ballTop -= 5;
+    } else if (dirTop == "down") {
+        ballTop += 5;
+    }
+
+    ball.style.left = ballLeft + "px";
+    ball.style.top = ballTop + "px";
+    animId = requestAnimationFrame(move);
+}
+
+// Start the animation
+animId = requestAnimationFrame(move);
