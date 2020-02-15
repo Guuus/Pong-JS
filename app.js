3aea619006631a8196e0a06102c42712ee11c6a7
Gus <gduigou29@gmail.com>
2020-02-15 17:51:59 +0100

    feat: interaction between the player 1 and the ball

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ class Player {
 
     move(keyCode) {
         if (keyCode == 38 && this.top > 0) {
-            this.top -= 25;
+            this.top -= 36;
             this.player.style.top = (this.top) + "px";
-        } else if (keyCode == 40 && this.top < 450) {
-            this.top += 25;
+        } else if (keyCode == 40 && this.top < 500) {
+            this.top += 36;
             this.player.style.top = (this.top) + "px";
         }
         console.log(this.top);
@@ -31,20 +31,29 @@ var ballTop = ball.offsetTop;
 var dirLeft = "up";
 var dirTop = "down";
 var animId = null;
+var scoreP1 = 0;
+var scoreP2 = 0;
+document.getElementById("scoreP2").textContent = scoreP2;
 
 function move() {
 
     // Move the ball in the good direction on the y axis
     if (ballLeft > 1345 && ballLeft < 1353) {
         dirLeft = "down";
+        scoreP1++;
+        document.getElementById("scoreP1").textContent = scoreP1;
     } else if (ballLeft == 0) {
+        dirLeft = "up";
+        scoreP2++;
+        document.getElementById("scoreP2").textContent = scoreP2;
+    } else if (ballLeft > 25 && ballLeft < 48 && ballTop >= (player1.top - 40) && ballTop <= (player1.top + 200)) {
         dirLeft = "up";
     }
     
     if (dirLeft == "up") {
-        ballLeft += 4;
+        ballLeft += 10;
     } else if (dirLeft == "down") {
-        ballLeft -= 4;
+        ballLeft -= 10;
     }
 
     // Move the ball in the good direction on the x axis
@@ -55,11 +64,12 @@ function move() {
     }
 
     if (dirTop == "up") {
-        ballTop -= 5;
+        ballTop -= 10;
     } else if (dirTop == "down") {
-        ballTop += 5;
+        ballTop += 10;
     }
 
+    console.log(ballLeft)
     ball.style.left = ballLeft + "px";
     ball.style.top = ballTop + "px";
     animId = requestAnimationFrame(move);
